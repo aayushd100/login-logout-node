7a0a64b57161fbc0635234bdc4d9e7b5581a6fb0
Aayush Dutt <aayushd100@gmail.com>
2018-01-26 13:26:40 +0000

    first commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,11 @@
+// /* global $ */
+// console.log('a');
+// $( "#submit2" ).on( "click", function( event ) {
+//   event.preventDefault();
+//   console.log('b');
+//   var data = $( this ).serialize() ;
+//   console.log(data+'g');
+//   $.post("/users/register", data);
+//   var a = $('#1').val();
+//   console.log(a);
+// })
